ef62d6d346080d428a4f897556a4d90170de5ebd
Weiran <huangweiran1998@gmail.com>
2019-11-13 17:09:29 +0800

    finish (without test) landmark energy

--- landmark.py
+++ landmark.py
@@ -1,1 +1,33 @@
-def 
+import numpy as np
+
+
+def landmark_loss(projected, face_index, landmark_index, points, weight, embedding, pose=True):
+    """
+    landmark energy.
+    :param embedding: n x 12. Normalized embedding matrix.
+    :param projected: 2 x n. The projected 2d point of all vertices.
+    :param indices: (n,). Indices of all front face vertices.
+    :param landmarks: (87,). 87 landmarks indices in 11510.
+    :param points: 87 x 2. the coordinates of landmarks.
+    :param weight:
+    :return:
+    """
+    mapped = dict(zip(face_index, np.arange(len(face_index))))
+    landmarks = [mapped[i] for i in landmark_index]  # map to the row of projected matrix
+    cor = projected[:, landmarks].T
+    diff = cor - points # n x 2
+    if pose:
+        w = np.dot(embedding, weight)[landmarks]  # n x 1
+        loss = w * np.linalg.norm(diff)
+    else:
+        loss = np.linalg.norm(diff)
+
+    return loss
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
--- run_model.py
+++ run_model.py
@@ -1,6 +1,9 @@
 import numpy as np
+import glob
+import os
 import cv2
 from load_all_mat import load_all_mat
+from landmark import landmark_loss
 
 class bcolors:
     HEADER = '\033[95m'
@@ -13,40 +16,79 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+def build_model(core, beta, embedding, rt):
+    """
+    From 3d to 2d.
+    :param core: (3xn) x exp_num [here (3x7794) x 47]
+    :param beta: (exp_num - 1) x ncluster [here 46 x 12]
+    :param clusters: one-hot encoded, n x 12
+    :param rt: extrinsic matrix, 3 x 4
+    :return:
+    model: n x 3, the first 2 dims are (x,y), z indicates depth, for rendering use.
+    """
+    scale = 160.53447   # TODO： 这个是从之前matlab代码求出来的前20张图片的f的平均值
+
+    face = np.expand_dims(core[:, 0], 1) + np.dot(core[:, 1:], beta)  # (3 x n) x 12
+    face = np.sum(face * np.repeat(embedding, 3, axis=0), axis=1).reshape(-1, 3).T  # n x 3
+    face = np.vstack((face, np.ones((1, face.shape[1]))))  # n x 4
+    # scale * rotation * face + tranlation
+    rt[:,:3] *= scale
+    model = np.dot(rt, face)
+
+    return model
+
+
 # 1. load segmentation result
-# current unsolved problems:
-# 1.1 数据。之前缺少连续帧mesh的数据，TODO: 之后可以用祥哥那个生成的视频的结果来重新做一次。
-# currently use the former results.
-face_index = np.load('/Users/momo/Desktop/region_seg/face_index.npy')
-clusters = np.load('/Users/momo/Desktop/region_seg/clusters.npy')
+face_index = np.load('final_face_index.npy')  # 7794
+embedding = np.load('embedding.npy')  # 7794 x 12, normalized embedding matrix
 n_cluster = 12
+weight = np.ones((n_cluster, 1))  # todo: change to dynamic weight
 
 # 2. region-based multi-linear face model
-
-# 2.1 gather data
+# 2.1 gather base data
 print(bcolors.OKGREEN + "[Loading data] load_all_mat..." + bcolors.ENDC)
 blendshapes, index_new87, mean_face, cr, single_value, w_exp_initial, \
-w_id_initial, triangles = load_all_mat(from_npy=True)
+w_id_initial, triangles = load_all_mat(from_npy=True)  # core: 34530 x 50 x 47
 print(bcolors.OKBLUE + "[Loaded] Done!" + bcolors.ENDC)
 
-shape_weight = np.load('/Users/momo/Desktop/face_model/para_id.npy')  # result from another model
 
-core = np.tensordot(cr, shape_weight, axes=(1,0)).squeeze()
+landmarks = index_new87.squeeze() - 1
+# TODO: learn shape weight from online adaptation method
+shape_weight = np.load('/Users/momo/Desktop/face_model/para_id.npy')  # 50 x 1, result from another model
+
+core = np.tensordot(cr, shape_weight, axes=(1,0)).squeeze()  # 34530 x 47
+
 keys = np.vstack((3 * face_index, 3 * face_index + 1, 3 * face_index + 2)).T
 keys = keys.flatten()
-print(keys.shape)
+
 core[:, 1:] = core[:, 1:] - np.expand_dims(core[:, 0], 1)  # convert to delta
-core = core[keys, :]  # (3 x 5913) x 47
+core = core[keys, :]  # (3 x 7794) x 47
 
 # 2.2 model parameters
-beta = np.random.random((46, n_cluster))  # expression parameters
-rot = np.random.random((3,3))  # pose parameters: rotation
-translate = np.random.random((1,3))  # pose parameters: translation
+# 2.2 gather pic data
+root_path = '/Users/momo/Desktop/test_frames/test_video_frames'
+pic_names = sorted(glob.glob(os.path.join(root_path, '*.jpeg')))
+pt_names = sorted(glob.glob(os.path.join(root_path, '*lds87.txt')))
+print(bcolors.OKGREEN + f'[Counted] Total pic number is {len(pic_names)}' + bcolors.ENDC)
+
+beta = np.random.random((46, n_cluster))  # 46 x 12 expression parameters
+rt = np.random.random((3, 4))  # pose parameters: [R|t] extrinsic matrix
+
 
 # 2.3 build model
-face = np.expand_dims(core[:, 0], 1) + np.dot(core[:, 1:], beta)
-print(face.shape)
+for i in range(len(pic_names)):
+    print(bcolors.BOLD + f'[Processing] pic number {i + 1}...' + bcolors.ENDC)
+    points = np.loadtxt(pt_names[i])  # 87 x 2
 
+    beta = np.random.random((46, n_cluster))  # 46 x 12 expression parameters
+    rt = np.random.random((3, 4))  # pose parameters: [R|t] extrinsic matrix
+
+    model = build_model(core, beta, embedding, rt)
+    projected = model[:2, :]
+
+    loss = landmark_loss(projected, face_index, landmarks, points, weight, embedding, pose=False)
+    print(loss)
+    break
 
 
 
